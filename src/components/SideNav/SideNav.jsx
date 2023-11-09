63b1941499ef35b96015de70d729fd21de7b6301
arafat-sani <ffsani029@gmail.com>
2023-11-09 21:53:14 +0600

    some modifie

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -3,6 +3,7 @@ import './SideNav.css'
 import { BiHomeSmile, BiSolidVideos } from "react-icons/bi";
 import { RxDashboard } from "react-icons/rx";
 import { PiDotsNineBold, PiTelegramLogoDuotone} from "react-icons/pi";
+import Link from 'next/link';
 const SideNav = () => {
     return (
         <div className='sideNav-container h-[100%]'>
@@ -11,19 +12,19 @@ const SideNav = () => {
            <div className='hidden md:block lg:block'>
             <div className="SideNav h-[100vh] flex flex-col justify-center items-center cursor-pointer">
                     <div className='SideNavItem w-[36px] h-[36px] my-2 duration-300 flex items-center'>
-                        <BiHomeSmile className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></BiHomeSmile>
+                       <Link href='/'><BiHomeSmile className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></BiHomeSmile></Link>
                     </div>
                     <div className='SideNavItem w-[36px] h-[36px] cursor-pointer my-2 duration-300 flex items-center'>
-                        <RxDashboard className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></RxDashboard>
+                       <Link href='/dashboard'><RxDashboard className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></RxDashboard></Link>
                     </div>
                     <div className='SideNavItem w-[36px] h-[36px] cursor-pointer my-2 duration-300 flex items-center'>
-                        <PiTelegramLogoDuotone className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></PiTelegramLogoDuotone>
+                       <Link href='/telegram'><PiTelegramLogoDuotone className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></PiTelegramLogoDuotone></Link>
                     </div>
                     <div className='SideNavItem w-[36px] h-[36px] cursor-pointer my-2 duration-300 flex items-center'>
-                        <BiSolidVideos className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></BiSolidVideos>
+                       <Link href='/videos'><BiSolidVideos className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></BiSolidVideos></Link>
                     </div>
                     <div className='SideNavItem w-[36px] h-[36px] cursor-pointer my-2 duration-300 flex items-center'>
-                        <PiDotsNineBold className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></PiDotsNineBold>
+                        <Link href='/more'><PiDotsNineBold className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></PiDotsNineBold></Link> 
                     </div>
                 </div>
            </div>
@@ -34,19 +35,19 @@ const SideNav = () => {
             <div className="block md:hidden fixed bottom-0 w-[100%] z-10">
                 <div className='SideNav flex w-[100%] justify-around items-center'>
                     <div className='SideNavItem w-[36px] h-[36px] my-2 duration-300 flex items-center'>
-                        <BiHomeSmile className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></BiHomeSmile>
+                       <Link href='/'><BiHomeSmile className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></BiHomeSmile></Link>
                     </div>
                     <div className='SideNavItem w-[36px] h-[36px] cursor-pointer my-2 duration-300 flex items-center'>
-                        <RxDashboard className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></RxDashboard>
+                       <Link href='/dashboard'><RxDashboard className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></RxDashboard></Link>
                     </div>
                     <div className='SideNavItem w-[36px] h-[36px] cursor-pointer my-2 duration-300 flex items-center'>
-                        <PiTelegramLogoDuotone className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></PiTelegramLogoDuotone>
+                       <Link href='/telegram'><PiTelegramLogoDuotone className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></PiTelegramLogoDuotone></Link>
                     </div>
                     <div className='SideNavItem w-[36px] h-[36px] cursor-pointer my-2 duration-300 flex items-center'>
-                        <BiSolidVideos className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></BiSolidVideos>
+                       <Link href='/videos'><BiSolidVideos className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></BiSolidVideos></Link>
                     </div>
                     <div className='SideNavItem w-[36px] h-[36px] cursor-pointer my-2 duration-300 flex items-center'>
-                        <PiDotsNineBold className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></PiDotsNineBold>
+                       <Link href='/more'><PiDotsNineBold className='SideNavIcon w-[22px] h-[22px] text-center mx-auto'></PiDotsNineBold></Link>
                     </div>
                 </div>
             </div>
